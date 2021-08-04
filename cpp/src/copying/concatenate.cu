f4b1b6bdb3af866f69a63d1628833abd7585f4ff
Dave Baranec <dbaranec@nvidia.com>
2021-08-04 10:33:57 -0500

    Formatting.

--- cpp/src/copying/concatenate.cu
+++ cpp/src/copying/concatenate.cu
@@ -374,17 +374,13 @@ void traverse_children::operator()<cudf::string_view>(host_span<column_view cons
   size_t const total_char_count = std::accumulate(
     cols.begin(), cols.end(), std::size_t{}, [stream](size_t a, auto const& b) -> size_t {
       strings_column_view scv(b);
-      return a + (b.is_empty()
-                    ? 0
-                    /*
-                    : cudf::detail::get_value<offset_type>(scv.offsets(), scv.offset() + b.size(), stream) -
-                        cudf::detail::get_value<offset_type>(scv.offsets(), scv.offset(), stream));
-                        */
-                    // if the column is unsliced, skip the offset retrieval.                    
-                    : scv.offset() > 0 ? 
-                        cudf::detail::get_value<offset_type>(scv.offsets(), scv.offset() + b.size(), stream) -
+      return a + (b.is_empty() ? 0
+                  // if the column is unsliced, skip the offset retrieval.
+                  : scv.offset() > 0
+                    ? cudf::detail::get_value<offset_type>(
+                        scv.offsets(), scv.offset() + b.size(), stream) -
                         cudf::detail::get_value<offset_type>(scv.offsets(), scv.offset(), stream)
-                          : scv.chars_size());
+                    : scv.chars_size());
     });
   // note:  output text must include "exceeds size_type range" for python error handling
   CUDF_EXPECTS(total_char_count <= static_cast<size_t>(std::numeric_limits<size_type>::max()),
